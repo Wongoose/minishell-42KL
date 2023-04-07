dcdbe76cdc7aeaf7edbf5855f9904d6a6fdfa83f
Zer <livefatezz@gmail.com>
2023-04-07 15:53:08 +0800

    heredoc expand dollar sign done

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -17,8 +17,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*newstr;
 	size_t	total_len;
 
-	if (!s1 || !s2)
-		return (0);
+	if (!s1)
+		return (ft_strdup(s2));
+	else if (!s2)
+		return (ft_strdup(s1));
 	total_len = ft_strlen(s1) + ft_strlen(s2) + 1;
 	newstr = (char *)ft_calloc(total_len, sizeof(char));
 	if (!newstr)
--- src/execution/heredoc_handler.c
+++ src/execution/heredoc_handler.c
@@ -6,7 +6,7 @@
 /*   By: chenlee <chenlee@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 14:46:29 by chenlee           #+#    #+#             */
-/*   Updated: 2023/04/06 20:57:55 by chenlee          ###   ########.fr       */
+/*   Updated: 2023/04/07 15:52:32 by chenlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,10 +51,7 @@ char	*check_existance(t_vars *vars, char *middle)
 		key_val = ft_split(vars->envp[i], '=');
 		if (ft_strncmp(key_val[0], middle, get_len(key_val[0], middle)) == 0)
 		{
-			printf("key_val[0]=[%s]\n", key_val[0]);
-			printf("key_val[1]=[%s]\n\n", key_val[1]);
 			ret = ft_strdup(key_val[1]);
-			printf("ret=[%s]\n\n", ret);
 			free_doublearray(key_val);
 			return (ret);
 		}
@@ -70,22 +67,14 @@ char	*check_and_replace(t_vars *vars, char *str, int start, int end)
 	char	*rear;
 	char	*replace;
 
-	// printf("start=%d\n", start);
-	// printf("lennn=%d\n", end);
-	// printf("strln-%d\n\n", (int)ft_strlen(str));
 	front = ft_substr(str, 0, start);
 	rear = ft_substr(str, end, ft_strlen(str) - end);
 	middle = ft_substr(str, start + 1, end - start - 1);
-	printf("front=[%s]\n", front);
-	printf("middle=[%s]\n", middle);
-	printf("rearr=[%s]\n", rear);
-	
 	free(str);
 	replace = check_existance(vars, middle);
 	replace = replace_str(front, replace, rear);
 	str = ft_strdup(replace);
 	free(replace);
-	printf("str=[%s]\n\n", str);
 	return (str);
 }
 
@@ -100,7 +89,7 @@ char	*expand_dollar(t_vars *vars, char *str)
 		if (str[i] == '$' && (str[i + 1] != 0 || str[i + 1] != ' '))
 		{
 			j = i + 1;
-			while (str[j] != ' ')
+			while (str[j] != '\0' && str[j] != ' ')
 				j++;
 			str = check_and_replace(vars, str, i, j);
 		}
@@ -108,16 +97,17 @@ char	*expand_dollar(t_vars *vars, char *str)
 	return (str);
 }
 
-char	*get_readline(char *rdr_str)
+char	*get_readline(t_vars *vars, char *rdr_str)
 {
 	char	*temp_one;
 	char	*temp_two;
 	char	*str;
 
-	str = ft_strdup("");
+	str = NULL;
 	while (1)
 	{
 		temp_one = readline("> ");
+		temp_one = expand_dollar(vars, temp_one);
 		if (temp_one == NULL)
 			return (NULL);
 		if (ft_strncmp(temp_one, rdr_str, get_len(temp_one, rdr_str)) == 0)
@@ -125,7 +115,8 @@ char	*get_readline(char *rdr_str)
 		temp_two = ft_strjoin(temp_one, "\n");
 		free(temp_one);
 		temp_one = ft_strjoin(str, temp_two);
-		free(str);
+		if (str != NULL)
+			free(str);
 		free(temp_two);
 		str = ft_strdup(temp_one);
 		free(temp_one);
@@ -144,13 +135,10 @@ char	*handle_per_cmdlst(t_vars *vars, t_pipe cmdlst)
 	{
 		if (cmdlst.rdr_info[i].rdr_type == HEREDOC)
 		{
-			if (ret != NULL)
-				free(ret);
-			ret = get_readline(cmdlst.rdr_info[i].rdr_str);
+			free(ret);
+			ret = get_readline(vars, cmdlst.rdr_info[i].rdr_str);
 			if (ret == NULL)
 				return (NULL);
-			ret = expand_dollar(vars, ret);
-			printf("ret=%s\n", ret);
 		}
 	}
 	return (ret);
